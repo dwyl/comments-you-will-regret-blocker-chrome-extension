edbcb3abaf84fd9cd0b4e7e9ede4cb172e0848b0
Ryan P.C. McQuen <ryan.q@linux.com>
2016-12-21 06:57:14 -0800

    Add initial boilerplate.
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

---
+++ regretBlocker.js
@@ -0,0 +1,9 @@
+// @flow
+window.addEventListener('DOMContentLoaded', () => {
+  const textareas = [...document.querySelectorAll('textarea')]
+  textareas.map((textarea) => {
+    textarea.addEventListener('change', () => {
+      console.log(textarea.split())
+    })
+  })
+})
